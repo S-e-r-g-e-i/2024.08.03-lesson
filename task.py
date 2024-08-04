0875c3be04fd90720e57cc6d95390b6d64e7789f
S-e-r-g-e-i <s.ya.aronov.b@gmail.com>
2024-08-04 09:24:35 +0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,6 @@
+# Задание по теме "Переменные"
+number_homework_completed = 12
+number_spent_hours = 1.5
+course_name = 'Phyton'
+time_one_task = number_spent_hours / number_homework_completed
+print('Курс:', course_name, ', всего задач:', number_homework_completed, ', затрачено часов:', number_spent_hours, ', среднее время выполнения', time_one_task, 'часа.')
